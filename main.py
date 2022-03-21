5c9f67d441e1de45a63b89a3c9e9ef442c858d05
Abhijeet kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2022-03-21 20:05:54 +0530

    version1.2
    updated source code for the application

--- main.py
+++ main.py
@@ -1,92 +1,72 @@
-
-from PIL import ImageGrab # to capture rapid screenshot
-import pyautogui # For Cursor Coordinates
 from tkinter import * # all
+import pyautogui # For Cursor Coordinates
+from PIL import ImageGrab # to capture rapid screenshot
+import webbrowser
+import clipboard as pc
 
 
 root=Tk() #call tkinter function
-root.geometry("630x650") #Width x Height
+root.geometry("380x480") #Width x Height 
 root.resizable(False,False) # window always stay in same size 
-root.title("On-Screen RGB Color Detector ") #Title bra
+root.title("On-Screen RGB Color Detector ") #Title bar
 root.configure(background='white')
 
-
-
 # Creating object of photoimage class
-p1= PhotoImage(file = 'Images/RGBwincon.png')
+p1= PhotoImage(file = 'RGBwincon.png')
 root.iconphoto(False, p1)# Setting icon of master window
-
 
 
 # Controle Structure/Logic----------------------------------------------------------------------------
 
 
+def Capture_RGB(event): # main function to capture RGB values from live screen
 
-Value=str('START') # Global varible to break while loop
+    if event.keysym=='c': # check c key is pressed ?
 
+        print('Key press detected, RGB values :')
 
-
-def stop(*args): #function to get Selected value from list DropDown menu ( show_selected, button command)
-    
-        global Value # Passing global variables data between functions
+        pos = pyautogui.position() # Varible to store Cursore value
+        pixelRGB = ImageGrab.grab().getpixel((pos)) # pass coordinate as x,y (pos)
         
-        Value = 'STOP'
+        print(pos,pixelRGB)
         
-        print('stop Function value ', Value) # for shell
-    
+
+        pc.copy(f" {pixelRGB}" ) # auto copy RGB values to clipboard/keyboard , tuple data type to string
+
+        v = pc.paste() # for shell
+        print('clipboard : ',v)
+
+        #Push Cursor Coordinates & RGB value to GUI labels
+
+        L1=Label(text="Cursor Coordinates , RGB Values ",font=('Helvetica',14,'bold'),width=30, height=4)
+        L1.place(x=7,y=140)
+        L2=Label(text=f" {pos},{pixelRGB} ",font=('Helvetica',14,'bold'),width=30, height=3)
+        L2.place(x=7,y=200)
+
+        L3=Label(root,text='Copied to clipboard \u2713',font=('Helvetica',10,'bold'), bg="white") #static label
+        L3.place(x=120,y=280)
+
+        
+def open_link(*args): #function binded to clickable hyperlink label
+    webbrowser.open_new("https://github.com/Abhijeetbyte/On-Screen-RGB-Detector")
+
    
-        
-def show_RGB(*args):#function to get value of Cursor Coordinates & RGB in every 1000ms
-
-        global Value # Passing global variables data between functions
-
-        print('RGB Function value ',Value) # for shell
-        
-
-        if Value == 'START':
-
-                pos = pyautogui.position() # Varible to store Cursore value
-                pixelRGB = ImageGrab.grab().getpixel((pos)) # pass coordinate as x,y (pos)
-
-                print(pos,pixelRGB)
- 
-                #Push Cursor Coordinates & RGB value to GUI labels
-    
-                L6=Label(text="[Cursor Coordinates]  [ RGB Values ]",font=('Helvetica',14,'bold'),bg="#f0f0f0",width=40, height=4).place(x=70,y=350)
+  
+# Graphical User Interface (GUI)--------------------------------------------------------------------------------
 
 
-                L7=Label(text=f"{pos,pixelRGB} ",font=('Helvetica',14,'bold'),bg="#f0f0f0",width=40, height=3).place(x=70,y=420)
-
-                L1=Label(text=" Values update in every 3 seconds !",font=("Courier", 12),fg='red',bg="white").place(x=120,y=520) #Text, FontSize/style, bg color, Location ( instruction )
-
-
-                
-                root.after(3000, show_RGB)# Tkinter has it own while true loop (root.mainloop). to run another while loop function
-                                  # use this (before mainloop) and that function have to run the same after(...) to work in loop.
-                                  # root.after(time_ms, function_name_without_brackets )
-
-        Value = 'START' # Preset the value for next loop
-
-        
-
-                
-# Graphical User Interface (GUI)---------------------------------------------------------------------------------------------------------                                        
-
-
-
-# Labels
-L0=Label(root,font=('Helvetica',15,'bold'),text="ON-SCREEN RGB COLOR DETECTOR ", borderwidth=3, relief="raised",justify=CENTER,bg="#FFDA2F",width=100)#FontSize/style, bg color(Head Label)
+# Labels 
+L0=Label(root,font=('Helvetica',15,'bold'),text="ON-SCREEN RGB COLOR DETECTOR ", borderwidth=3, relief="raised",justify=CENTER,bg="#FFDA2F",width=100) #Font Size/style, bg color (Head Label)
 L0.pack(padx=2)
 
-L1=Label(text="R,G,B = 255,255,255 (Absolute White)",font=("Courier", 12, "italic"),bg="white").place(x=120,y=70) #Text, FontSize/style, bg color, Location ( instruction )
-L2=Label(text="R,G,B = 0,  0,  0   (Absolute Black)",font=("Courier", 12, "italic"),bg="white").place(x=120,y=120)
-L3=Label(text="Respectively For All Colors :",font=("Courier", 12, "italic"),bg="white").place(x=150,y=170)
-L4=Label(text="R or G or B is (255, Represent Maximum Amount of that Color)",font=("Courier", 12, "italic"),bg="white").place(x=10,y=230)
-L5=Label(text="R or G or B is (  0, Represent Minimum Amount Of that Color)",font=("Courier", 12, "italic"),bg="white").place(x=10,y=280) 
+L4=Label(text="Point your mouse circure and \n\npress 'C' on keyboard to capture ",font=('Helvetica',12,'bold'),bg="white") #instruction label
+L4.place(x=60,y=350) #Text, FontSize/style, bg color, Location
+
+L5=Label(root,text=' \u24D8 About ', fg='lightblue', font=('Helvetica',12,'bold'), borderwidth=0.5, relief="sunken",bg="white") # open link (text label)
+L5.place(x=10,y=60) # webbrowser link label position
+L5.bind("<Button-1>", open_link)
 
 #Buttons
-Show_Button=Button(text ="Stop",font=('Helvetica',12,'bold'), bg="#FFDA2F",fg="black",width=8,height=1, command = stop).place(x=80,y=570) #Clear All command button & location 
-Stop_Button=Button(text ="Start",font=('Helvetica',12,'bold'), bg="#FFDA2F",fg="black",width=8,height=1, command = show_RGB).place(x=450,y=570) #Calculte Volume command button & location 
-
+root.bind('<KeyPress>', Capture_RGB) #Keyboard key as command to execute function
 
 root.mainloop() #Execute tkinter
